ef6b5652c352daa00685e2cc3fda74bafb7e85d7
Snehesh Gupta <snehesh50.gupta@gmail.com>
2024-10-02 21:28:17 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,17 @@
+function check() {
+    var c=0;
+    var q1=document.quiz.question1.value;
+    var q2=document.quiz.question2.value;
+    var q3=document.quiz.question3.value;
+    var q4=document.quiz.question4.value;
+    var q5=document.quiz.question5.value;
+    if (q1=="Messi") {c++}
+    if (q2=="Dhoni") {c++}
+    if (q3=="Dhyan Chand") {c++}
+    if (q4=="LeBron James") {c++}
+    if (q5=="Muhammad Ali") {c++}
+
+        document.write("Your Score"+3);
+
+        
+}
